969034c5bf41bf7734a48f46e30ecbaf913cc682
Nima Golzar Soufiani <nima.soufiani@rippleenergy.com>
2022-10-01 22:57:02 +0100

    add logic in store secrets function to store secret

--- store-secrets/app.ts
+++ store-secrets/app.ts
@@ -12,87 +12,98 @@ export const lambdaHandler = async (event: SQSEvent, context: Context) => {
     const end = event.Records.length;
     try {
         while (iterator < end) {
-            const userName = event.Records[iterator].messageAttributes.UserName.stringValue;
-            const createUserAccessKey = await iam.createAccessKey({ UserName: userName }).promise();
+            const SecretId = event.Records[iterator].messageAttributes.SecretId.stringValue;
 
+            if (SecretId) {
+                // const createUserAccessKey = await iam.createAccessKey({ UserName: userName }).promise();
 
-            // console.log('Getting the secret');
-            // try {
-            //     await secretsManager
-            //         .getSecretValue({
-            //             SecretId: `${userName}-access-key`,
-            //         })
-            //         .promise();
-            //     await secretsManager
-            //         .updateSecret({
-            //             SecretId: `${userName}-access-key`,
-            //             SecretString: JSON.stringify({
-            //                 accessKeyId: createUserAccessKey.AccessKey.AccessKeyId,
-            //                 secretAccessKey: createUserAccessKey.AccessKey.SecretAccessKey,
-            //             }),
-            //         })
-            //         .promise();
-            // } catch (error) {
-            //     try {
-            //         await secretsManager
-            //             .createSecret({
-            //                 Name: `${userName}-access-key`,
-            //                 SecretString: JSON.stringify({
-            //                     accessKeyId: createUserAccessKey.AccessKey.AccessKeyId,
-            //                     secretAccessKey: createUserAccessKey.AccessKey.SecretAccessKey,
-            //                 }),
-            //             })
-            //             .promise()
-            //             .then((data) => {
-            //                 console.log('Secret Created');
-            //                 secretsManager
-            //                     .putResourcePolicy({
-            //                         SecretId: `${userName}-access-key`,
-            //                         ResourcePolicy: JSON.stringify({
-            //                             Version: '2012-10-17',
-            //                             Statement: [
-            //                                 {
-            //                                     Sid: 'EnableIAMUserPermissions',
-            //                                     Effect: 'Allow',
-            //                                     Principal: {
-            //                                         AWS: `arn:aws:iam::${arn}:user/${userName}`,
-            //                                     },
-            //                                     Action: [
-            //                                         'secretsmanager:GetSecretValue',
-            //                                         'secretsmanager:DescribeSecret',
-            //                                         'secretsmanager:ListSecretVersionIds',
-            //                                         'secretsmanager:GetResourcePolicy',
-            //                                     ],
-            //                                     Resource: data.ARN,
-            //                                 },
-            //                                 {
-            //                                     Sid: 'AllowListingOfSecrets',
-            //                                     Effect: 'Allow',
-            //                                     Principal: {
-            //                                         AWS: `arn:aws:iam::${arn}:user/${userName}`,
-            //                                     },
-            //                                     Action: [
-            //                                         'secretsmanager:GetRandomPassword',
-            //                                         'secretsmanager:ListSecrets',
-            //                                     ],
-            //                                     Resource: '*',
-            //                                 },
-            //                             ],
-            //                         }),
-            //                     })
-            //                     .promise();
-            //                 console.log('Secrets Manager Resource Policy Updated');
-            //                 console.log('Secret Created and Resource Policy Updated');
-            //             })
-            //             .catch((error) => {
-            //                 console.log('error creating secret', error);
-            //             });
-            //         // TODO: Get old access key and delete it
-            //     } catch (error) {
-            //         console.log('Error during creation of secret and attaching resource policy');
-            //         console.log(error);
-            //     }
-            // }
+                // console.log('Getting the secret');
+                const messageAttributesObject = Object.entries(event.Records[iterator].messageAttributes).reduce(
+                    (acc, [key, value]) => {
+                        if (key === 'SecretId' || key == 'Principle') {
+                            return {};
+                        }
+                        return { ...acc, [key]: value.stringValue };
+                    },
+                    {},
+                );
+                try {
+                    await secretsManager
+                        .getSecretValue({
+                            SecretId,
+                        })
+                        .promise();
+
+                    await secretsManager
+                        .updateSecret({
+                            SecretId,
+                            SecretString: JSON.stringify({
+                                messageAttributesObject,
+                            }),
+                        })
+                        .promise();
+                } catch (error) {
+                    try {
+                        await secretsManager
+                            .createSecret({
+                                Name: SecretId,
+                                SecretString: JSON.stringify({
+                                    messageAttributesObject,
+                                }),
+                            })
+                            .promise()
+                            .then((data) => {
+                                console.log('Secret Created');
+                                secretsManager
+                                    .putResourcePolicy({
+                                        SecretId,
+                                        ResourcePolicy: JSON.stringify({
+                                            Version: '2012-10-17',
+                                            Statement: [
+                                                {
+                                                    Sid: 'EnableIAMUserPermissions',
+                                                    Effect: 'Allow',
+                                                    Principal: {
+                                                        AWS: `arn:aws:iam::${arn}:user/${event.Records[iterator].messageAttributes.Principle}`,
+                                                    },
+                                                    Action: [
+                                                        'secretsmanager:GetSecretValue',
+                                                        'secretsmanager:DescribeSecret',
+                                                        'secretsmanager:ListSecretVersionIds',
+                                                        'secretsmanager:GetResourcePolicy',
+                                                    ],
+                                                    Resource: data.ARN,
+                                                },
+                                                {
+                                                    Sid: 'AllowListingOfSecrets',
+                                                    Effect: 'Allow',
+                                                    Principal: {
+                                                        AWS: `arn:aws:iam::${arn}:user/${event.Records[iterator].messageAttributes.Principle}`,
+                                                    },
+                                                    Action: [
+                                                        'secretsmanager:GetRandomPassword',
+                                                        'secretsmanager:ListSecrets',
+                                                    ],
+                                                    Resource: '*',
+                                                },
+                                            ],
+                                        }),
+                                    })
+                                    .promise();
+                                console.log('Secrets Manager Resource Policy Updated');
+                                console.log('Secret Created and Resource Policy Updated');
+                            })
+                            .catch((error) => {
+                                console.log('error creating secret', error);
+                            });
+                        // TODO: Get old access key and delete it
+                    } catch (error) {
+                        console.log('Error during creation of secret and attaching resource policy');
+                        console.log(error);
+                    }
+                }
+            }
+
             iterator++;
         }
     } catch (error) {
