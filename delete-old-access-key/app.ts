404c47332d6a377a6444cdbf39669d24dcaadc52
Nima Golzar Soufiani <nima.soufiani@rippleenergy.com>
2022-10-04 16:23:40 +0100

    create sns topic

--- delete-old-access-key/app.ts
+++ delete-old-access-key/app.ts
@@ -1,39 +1,40 @@
-import { Context, SQSEvent } from 'aws-lambda';
+import { Context, SNSEvent } from 'aws-lambda';
 
-import AWS from 'aws-sdk';
+// import AWS from 'aws-sdk';
 
-const iam = new AWS.IAM();
+// const iam = new AWS.IAM();
 
-export const lambdaHandler = async (event: SQSEvent, context: Context) => {
-    let iterator = 0;
-    const end = event.Records.length;
+export const lambdaHandler = async (event: SNSEvent, context: Context) => {
+    console.log(event);
+    // let iterator = 0;
+    // const end = event.Records.length;
 
-    while (iterator < end) {
-        const userName = event.Records[0].messageAttributes.UserName.stringValue;
+    // while (iterator < end) {
+    //     const userName = event.Records[0].messageAttributes.UserName.stringValue;
 
-        try {
-            const accessKeysForUser = await iam.listAccessKeys({ UserName: userName }).promise();
+    //     try {
+    //         const accessKeysForUser = await iam.listAccessKeys({ UserName: userName }).promise();
 
-            if (accessKeysForUser.AccessKeyMetadata.length > 0) {
-                const sortedAccessKeys = accessKeysForUser.AccessKeyMetadata.sort((a, b) => {
-                    if (a.CreateDate && b.CreateDate) {
-                        return b.CreateDate.getTime() - a.CreateDate.getTime();
-                    } else {
-                        return 0;
-                    }
-                });
+    //         if (accessKeysForUser.AccessKeyMetadata.length > 0) {
+    //             const sortedAccessKeys = accessKeysForUser.AccessKeyMetadata.sort((a, b) => {
+    //                 if (a.CreateDate && b.CreateDate) {
+    //                     return b.CreateDate.getTime() - a.CreateDate.getTime();
+    //                 } else {
+    //                     return 0;
+    //                 }
+    //             });
 
-                const accessKeysToDelete = sortedAccessKeys.slice(1);
+    //             const accessKeysToDelete = sortedAccessKeys.slice(1);
 
-                for (const accessKey of accessKeysToDelete) {
-                    if (accessKey.AccessKeyId) {
-                        await iam.deleteAccessKey({ AccessKeyId: accessKey.AccessKeyId, UserName: userName }).promise();
-                    }
-                }
-            }
-        } catch (error) {
-            console.log(error);
-        }
-        iterator++;
-    }
+    //             for (const accessKey of accessKeysToDelete) {
+    //                 if (accessKey.AccessKeyId) {
+    //                     await iam.deleteAccessKey({ AccessKeyId: accessKey.AccessKeyId, UserName: userName }).promise();
+    //                 }
+    //             }
+    //         }
+    //     } catch (error) {
+    //         console.log(error);
+    //     }
+    //     iterator++;
+    // }
 };
