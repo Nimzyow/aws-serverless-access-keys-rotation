c9de10bb13564324e95b8985d12538e5d38e4784
Nima Golzar Soufiani <nima.soufiani@rippleenergy.com>
2022-10-06 11:04:09 +0100

    refactor user-lookup and send sns message

--- store-secrets/app.ts
+++ store-secrets/app.ts
@@ -1,135 +1,134 @@
 import { Context, SQSEvent } from 'aws-lambda';
 
 import AWS from 'aws-sdk';
-// const sqs = new AWS.SQS();
+const sns = new AWS.SNS();
 
 const secretsManager = new AWS.SecretsManager();
 
-// const sendSQSMessage = async ({ region, account, userName }: { region: string; account: string; userName: string }) => {
-//     const params: AWS.SQS.SendMessageRequest = {
-//         QueueUrl: `https://sqs.${region}.amazonaws.com/${account}/DeleteOldAccessKeyQueue`,
-//         MessageAttributes: {
-//             UserName: {
-//                 DataType: 'String',
-//                 StringValue: userName,
-//             },
-//         },
-//         MessageBody: 'Delete old access keys',
-//     };
-//     const response = await sqs.sendMessage(params).promise();
-//     console.log('message sent with id of ' + response.MessageId);
-// };
+const sendSNSNotification = async ({
+    region,
+    account,
+    userName,
+}: {
+    region: string;
+    account: string;
+    userName: string;
+}) => {
+    const snsParams = {
+        Message: `User ${userName} has been updated`,
+        TopicArn: `arn:aws:sns:${region}:${account}:UpdatedAccessKeyAndStoredSecret`,
+    };
+
+    const response = await sns.publish(snsParams).promise();
+
+    console.log('notification sent with id of ' + response.MessageId);
+};
 
 export const lambdaHandler = async (event: SQSEvent, context: Context) => {
     const account = context.invokedFunctionArn.split(':')[4];
 
     let iterator = 0;
     const end = event.Records.length;
-    try {
-        while (iterator < end) {
-            const SecretId = event.Records[iterator].messageAttributes.SecretId.stringValue;
-            const userName = event.Records[iterator].messageAttributes.Principle.stringValue;
 
-            if (SecretId && userName) {
-                const messageAttributesObject = Object.entries(event.Records[iterator].messageAttributes).reduce(
-                    (acc, [key, value]) => {
-                        if (key === 'SecretId' || key == 'Principle') {
-                            return {};
-                        }
-                        return { ...acc, [key]: value.stringValue };
-                    },
-                    {},
-                );
+    while (iterator < end) {
+        const SecretId = event.Records[iterator].messageAttributes.SecretId.stringValue;
+        const userName = event.Records[iterator].messageAttributes.Principle.stringValue;
+
+        if (SecretId && userName) {
+            const messageAttributesObject = Object.entries(event.Records[iterator].messageAttributes).reduce(
+                (acc, [key, value]) => {
+                    if (key === 'SecretId' || key == 'Principle') {
+                        return {};
+                    }
+                    return { ...acc, [key]: value.stringValue };
+                },
+                {},
+            );
+            try {
+                await secretsManager
+                    .getSecretValue({
+                        SecretId,
+                    })
+                    .promise();
+
+                await secretsManager
+                    .updateSecret({
+                        SecretId,
+                        SecretString: JSON.stringify({
+                            messageAttributesObject,
+                        }),
+                    })
+                    .promise();
+                await sendSNSNotification({
+                    account,
+                    region: event.Records[iterator].awsRegion,
+                    userName,
+                });
+            } catch (error) {
                 try {
                     await secretsManager
-                        .getSecretValue({
-                            SecretId,
-                        })
-                        .promise();
-
-                    await secretsManager
-                        .updateSecret({
-                            SecretId,
+                        .createSecret({
+                            Name: SecretId,
                             SecretString: JSON.stringify({
                                 messageAttributesObject,
                             }),
                         })
-                        .promise();
-                    // await sendSQSMessage({
-                    //     account,
-                    //     region: event.Records[iterator].awsRegion,
-                    //     userName,
-                    // });
+                        .promise()
+                        .then((data) => {
+                            console.log('Secret Created');
+                            secretsManager
+                                .putResourcePolicy({
+                                    SecretId,
+                                    ResourcePolicy: JSON.stringify({
+                                        Version: '2012-10-17',
+                                        Statement: [
+                                            {
+                                                Sid: 'EnableIAMUserPermissions',
+                                                Effect: 'Allow',
+                                                Principal: {
+                                                    AWS: `arn:aws:iam::${account}:user/${event.Records[iterator].messageAttributes.Principle.stringValue}`,
+                                                },
+                                                Action: [
+                                                    'secretsmanager:GetSecretValue',
+                                                    'secretsmanager:DescribeSecret',
+                                                    'secretsmanager:ListSecretVersionIds',
+                                                    'secretsmanager:GetResourcePolicy',
+                                                ],
+                                                Resource: data.ARN,
+                                            },
+                                            {
+                                                Sid: 'AllowListingOfSecrets',
+                                                Effect: 'Allow',
+                                                Principal: {
+                                                    AWS: `arn:aws:iam::${account}:user/${event.Records[iterator].messageAttributes.Principle.stringValue}`,
+                                                },
+                                                Action: [
+                                                    'secretsmanager:GetRandomPassword',
+                                                    'secretsmanager:ListSecrets',
+                                                ],
+                                                Resource: '*',
+                                            },
+                                        ],
+                                    }),
+                                })
+                                .promise();
+                            console.log('Secrets Manager Resource Policy Updated');
+                            console.log('Secret Created and Resource Policy Updated');
+                        })
+                        .catch((error) => {
+                            console.log('error creating secret', error);
+                        });
+                    await sendSNSNotification({
+                        account,
+                        region: event.Records[iterator].awsRegion,
+                        userName,
+                    });
                 } catch (error) {
-                    try {
-                        await secretsManager
-                            .createSecret({
-                                Name: SecretId,
-                                SecretString: JSON.stringify({
-                                    messageAttributesObject,
-                                }),
-                            })
-                            .promise()
-                            .then((data) => {
-                                console.log('Secret Created');
-                                secretsManager
-                                    .putResourcePolicy({
-                                        SecretId,
-                                        ResourcePolicy: JSON.stringify({
-                                            Version: '2012-10-17',
-                                            Statement: [
-                                                {
-                                                    Sid: 'EnableIAMUserPermissions',
-                                                    Effect: 'Allow',
-                                                    Principal: {
-                                                        AWS: `arn:aws:iam::${account}:user/${event.Records[iterator].messageAttributes.Principle.stringValue}`,
-                                                    },
-                                                    Action: [
-                                                        'secretsmanager:GetSecretValue',
-                                                        'secretsmanager:DescribeSecret',
-                                                        'secretsmanager:ListSecretVersionIds',
-                                                        'secretsmanager:GetResourcePolicy',
-                                                    ],
-                                                    Resource: data.ARN,
-                                                },
-                                                {
-                                                    Sid: 'AllowListingOfSecrets',
-                                                    Effect: 'Allow',
-                                                    Principal: {
-                                                        AWS: `arn:aws:iam::${account}:user/${event.Records[iterator].messageAttributes.Principle.stringValue}`,
-                                                    },
-                                                    Action: [
-                                                        'secretsmanager:GetRandomPassword',
-                                                        'secretsmanager:ListSecrets',
-                                                    ],
-                                                    Resource: '*',
-                                                },
-                                            ],
-                                        }),
-                                    })
-                                    .promise();
-                                console.log('Secrets Manager Resource Policy Updated');
-                                console.log('Secret Created and Resource Policy Updated');
-                            })
-                            .catch((error) => {
-                                console.log('error creating secret', error);
-                            });
-                        // await sendSQSMessage({
-                        //     account,
-                        //     region: event.Records[iterator].awsRegion,
-                        //     userName,
-                        // });
-                    } catch (error) {
-                        console.log('Error during creation of secret and attaching resource policy');
-                        console.log(error);
-                    }
+                    console.log('Error during creation of secret and attaching resource policy');
+                    console.log(error);
                 }
             }
-
-            iterator++;
         }
-    } catch (error) {
-        console.log("Error while creating the user's access key");
-        console.log(error);
+        iterator++;
     }
 };
--- user-lookup/app.ts
+++ user-lookup/app.ts
@@ -4,72 +4,6 @@ import AWS from 'aws-sdk';
 const iam = new AWS.IAM();
 const sqs = new AWS.SQS();
 
-/**
- *
- * event bridge - DONE
- *     |
- *     |
- *    \ /
- *     |
- * Lambda - UserLookup - triggered 1/day by event bridge - lambda list users and send username to SQS queue - DONE
- *     |
- *     |
- *    \ /
- *     |
- * SQS Queue - UserQueue - receives a message with username in message attribute - DONE
- *     |
- *     |
- *    \ /
- *     |
- * Lambda - UserAccessKeyLookup - Polls UserQueue - Checks for age of access key - DONE
- * - if age is < 90 days, return and terminate flow. if > 90 days, send access key id to SQS queue - DONE
- *     |
- *     |
- *    \ /
- *     |
- * SQS Queue - UpdateUserAccessKeyQueue - receives a message with access key id in message attribute - DONE
- *                                      - Include username in message attributes - DONE
- *     |
- *     |
- *    \ /
- *     |
- * Lambda - CreateUserAccessKey - Polls UpdateUserAccessKeyQueue - Create new access key - DONE
- *     |
- *     |
- *     |
- *    \ /
- *     |
- * SQS Queue - StoreSecretsQueue - receives a message with:
- *                                          secretId - DONE
- *                                          key of accessKeyId - value of accessKeyId - DONE
- *                                          key of secretAccessKey - value of secretAccessKey - DONE
- *     |
- *     |
- *    \ /
- *     |
- * Lambda - StoreSecrets - Polls StoreSecretsQueue - stores or updates secret with access key - DONE
- *
- *     |
- *     |
- *    \ /
- *     |
- * SNS - Topic UpdatedUserAccessKey - SES subscribes to this topic and sends an email to the user.
- *
- *     |                                                                                      |
- *     |                                                                                      |
- *    \ /                                                                                    \ /
- *     |                                                                                      |
- * SQS Queue - DeleteOldAccessKeyQueue - receives a message with:                     SQS Queue - SendEmailQueue - receives a message with:
- *                                       UserName - Done                              UserName
- *     |                                                                                  |
- *     |                                                                                  |
- *    \ /                                                                                \ /
- *     |                                                                                  |
- * Lambda - DeleteOldAccessKey - Polls DeleteOldAccessKeyQueue                      Lambda - SendEmail - Polls SendEmailQueue
- *                              - lookup access keys - DONE
- *                              - delete oldest access key - DONE
- */
-
 export const lambdaHandler = async (event: ScheduledEvent, context: Context) => {
     try {
         const listUsers = await iam.listUsers().promise();
@@ -97,5 +31,6 @@ export const lambdaHandler = async (event: ScheduledEvent, context: Context) => 
         }
     } catch (error) {
         console.log(error);
+        throw new Error("Couldn't send message to SQS");
     }
 };
