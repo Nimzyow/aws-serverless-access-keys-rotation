498927732560a2cddbd4e0dea646d7db4ecff619
Nima Golzar Soufiani <nima.soufiani@rippleenergy.com>
2022-10-06 09:29:28 +0100

    complete sending email

--- delete-old-access-key/app.ts
+++ delete-old-access-key/app.ts
@@ -1,40 +1,39 @@
 import { Context, SNSEvent } from 'aws-lambda';
 
-// import AWS from 'aws-sdk';
+import AWS from 'aws-sdk';
 
-// const iam = new AWS.IAM();
+const iam = new AWS.IAM();
 
 export const lambdaHandler = async (event: SNSEvent, context: Context) => {
-    console.log(event);
-    // let iterator = 0;
-    // const end = event.Records.length;
+    let iterator = 0;
+    const end = event.Records.length;
 
-    // while (iterator < end) {
-    //     const userName = event.Records[0].messageAttributes.UserName.stringValue;
+    while (iterator < end) {
+        const userName = event.Records[0].Sns.MessageAttributes.UserName.Value;
 
-    //     try {
-    //         const accessKeysForUser = await iam.listAccessKeys({ UserName: userName }).promise();
+        try {
+            const accessKeysForUser = await iam.listAccessKeys({ UserName: userName }).promise();
 
-    //         if (accessKeysForUser.AccessKeyMetadata.length > 0) {
-    //             const sortedAccessKeys = accessKeysForUser.AccessKeyMetadata.sort((a, b) => {
-    //                 if (a.CreateDate && b.CreateDate) {
-    //                     return b.CreateDate.getTime() - a.CreateDate.getTime();
-    //                 } else {
-    //                     return 0;
-    //                 }
-    //             });
+            if (accessKeysForUser.AccessKeyMetadata.length > 0) {
+                const sortedAccessKeys = accessKeysForUser.AccessKeyMetadata.sort((a, b) => {
+                    if (a.CreateDate && b.CreateDate) {
+                        return b.CreateDate.getTime() - a.CreateDate.getTime();
+                    } else {
+                        return 0;
+                    }
+                });
 
-    //             const accessKeysToDelete = sortedAccessKeys.slice(1);
+                const accessKeysToDelete = sortedAccessKeys.slice(1);
 
-    //             for (const accessKey of accessKeysToDelete) {
-    //                 if (accessKey.AccessKeyId) {
-    //                     await iam.deleteAccessKey({ AccessKeyId: accessKey.AccessKeyId, UserName: userName }).promise();
-    //                 }
-    //             }
-    //         }
-    //     } catch (error) {
-    //         console.log(error);
-    //     }
-    //     iterator++;
-    // }
+                for (const accessKey of accessKeysToDelete) {
+                    if (accessKey.AccessKeyId) {
+                        await iam.deleteAccessKey({ AccessKeyId: accessKey.AccessKeyId, UserName: userName }).promise();
+                    }
+                }
+            }
+        } catch (error) {
+            console.log(error);
+        }
+        iterator++;
+    }
 };
--- send-email/app.ts
+++ send-email/app.ts
@@ -1,7 +1,58 @@
 import { Context, SQSEvent } from 'aws-lambda';
 
 import AWS from 'aws-sdk';
+import { PromiseResult } from 'aws-sdk/lib/request';
+const ses = new AWS.SES();
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 export const lambdaHandler = async (event: SQSEvent, context: Context) => {
-    console.log(JSON.stringify(event));
+    let iterator = 0;
+    const end = event.Records.length;
+
+    while (iterator < end) {
+        const userName = event.Records[iterator].messageAttributes.UserName.stringValue;
+        let user: PromiseResult<AWS.DynamoDB.DocumentClient.GetItemOutput, AWS.AWSError>;
+        try {
+            user = await dynamoDb
+                .get({
+                    TableName: 'User',
+                    Key: {
+                        name: userName,
+                    },
+                })
+                .promise();
+            console.log(user);
+        } catch (error) {
+            console.log(error);
+            throw new Error(`Couldn't get user: ${userName} from dynamodb`);
+        }
+        try {
+            // amend this line if you know the verified email address verified in SES and put it in the source field
+            const verifiedIdentities = await ses.listIdentities().promise();
+
+            const params = {
+                Destination: {
+                    ToAddresses: [user.Item?.email],
+                },
+                Message: {
+                    Body: {
+                        Text: {
+                            Charset: 'UTF-8',
+                            Data: `Hello ${userName},\n\nYour access key has been created.\n\nYour old access key has been invalidated. Go to secrets manager to view your new access key.\n\nThanks,\n\nThe Team`,
+                        },
+                    },
+                    Subject: {
+                        Charset: 'UTF-8',
+                        Data: 'Heads up - Access Key Created',
+                    },
+                },
+                Source: verifiedIdentities.Identities[0],
+            };
+            await new AWS.SES().sendEmail(params).promise();
+        } catch (error) {
+            console.log(error);
+            throw new Error(`Couldn't send email to user: ${userName}`);
+        }
+        iterator++;
+    }
 };
