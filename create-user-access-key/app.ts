b0a67a85b0ae7eb0d57bcf2a1480ac60f8af19fc
Nima Golzar Soufiani <nima.soufiani@rippleenergy.com>
2022-10-12 21:03:23 +0100

    move deletion of access key responsibility

--- create-user-access-key/app.ts
+++ create-user-access-key/app.ts
@@ -12,6 +12,33 @@ export const lambdaHandler = async (event: SQSEvent, context: Context) => {
 
     while (iterator < end) {
         const userName = event.Records[iterator].messageAttributes.UserName.stringValue
+
+        const accessKeysForUser = await iam.listAccessKeys({ UserName: userName }).promise()
+        try {
+            if (accessKeysForUser.AccessKeyMetadata.length > 0) {
+                const sortedAccessKeys = accessKeysForUser.AccessKeyMetadata.sort((a, b) => {
+                    if (a.CreateDate && b.CreateDate) {
+                        return b.CreateDate.getTime() - a.CreateDate.getTime()
+                    } else {
+                        return 0
+                    }
+                })
+
+                const accessKeysToDelete = sortedAccessKeys.slice(1)
+
+                for (const accessKey of accessKeysToDelete) {
+                    if (accessKey.AccessKeyId) {
+                        await iam
+                            .deleteAccessKey({ AccessKeyId: accessKey.AccessKeyId, UserName: userName })
+                            .promise()
+                    }
+                }
+            }
+        } catch (error) {
+            console.log(error)
+            throw new Error(`Couldn't delete oldest access key for user: ${userName}`)
+        }
+
         try {
             const createUserAccessKey = await iam.createAccessKey({ UserName: userName }).promise()
             await sqs
--- user-access-key-lookup/app.ts
+++ user-access-key-lookup/app.ts
@@ -8,8 +8,11 @@ const sqs = new AWS.SQS()
 const isKeyOutOfDate = ({
     createDate,
 }: {
-    createDate: NonNullable<IAM.AccessKeyMetadata["CreateDate"]>
+    createDate?: NonNullable<IAM.AccessKeyMetadata["CreateDate"]>
 }) => {
+    if (!createDate) {
+        return false
+    }
     const diff = new Date().getTime() - createDate.getTime()
     const diffDays = Math.ceil(diff / (1000 * 3600 * 24))
     return diffDays > 90
@@ -52,73 +55,38 @@ export const lambdaHandler = async (event: SQSEvent, context: Context) => {
 
     while (iterator < end) {
         const userName = event.Records[iterator].messageAttributes.UserName.stringValue
-        let listAccessKeysForUser: PromiseResult<IAM.ListAccessKeysResponse, AWS.AWSError>
+        let accessKeysForUser: PromiseResult<IAM.ListAccessKeysResponse, AWS.AWSError>
         try {
-            listAccessKeysForUser = await iam.listAccessKeys({ UserName: userName }).promise()
+            accessKeysForUser = await iam.listAccessKeys({ UserName: userName }).promise()
         } catch (error) {
             throw new Error(`Couldn't list access keys for user: ${userName}`)
         }
 
-        if (listAccessKeysForUser.AccessKeyMetadata.length > 1) {
-            if (
-                listAccessKeysForUser.AccessKeyMetadata[0].CreateDate &&
-                listAccessKeysForUser.AccessKeyMetadata[1].CreateDate &&
-                listAccessKeysForUser.AccessKeyMetadata[0].AccessKeyId &&
-                listAccessKeysForUser.AccessKeyMetadata[1].AccessKeyId &&
-                new Date(listAccessKeysForUser.AccessKeyMetadata[0].CreateDate) >
-                    new Date(listAccessKeysForUser.AccessKeyMetadata[1].CreateDate) &&
-                isKeyOutOfDate({ createDate: listAccessKeysForUser.AccessKeyMetadata[0].CreateDate })
-            ) {
-                try {
-                    await sendMessage({
-                        accessKeyId: listAccessKeysForUser.AccessKeyMetadata[0].AccessKeyId,
-                        userName: userName || "unknown",
-                        region: event.Records[iterator].awsRegion,
-                        awsAccountId: context.invokedFunctionArn.split(":")[4],
-                    })
-                } catch (error) {
-                    console.log(error)
-                    throw new Error("Couldn't send message to SQS")
-                }
-            } else if (
-                listAccessKeysForUser.AccessKeyMetadata[0].AccessKeyId &&
-                listAccessKeysForUser.AccessKeyMetadata[1].AccessKeyId &&
-                listAccessKeysForUser.AccessKeyMetadata[1].CreateDate &&
-                isKeyOutOfDate({
-                    createDate: listAccessKeysForUser.AccessKeyMetadata[1].CreateDate,
-                })
-            ) {
-                try {
-                    await sendMessage({
-                        accessKeyId: listAccessKeysForUser.AccessKeyMetadata[1].AccessKeyId,
-                        userName: userName || "unknown",
-                        region: event.Records[iterator].awsRegion,
-                        awsAccountId: context.invokedFunctionArn.split(":")[4],
-                    })
-                } catch (error) {
-                    console.log(error)
-                    throw new Error("Couldn't send message to SQS")
-                }
+        const sortedAccessKeys = accessKeysForUser.AccessKeyMetadata.sort((a, b) => {
+            if (a.CreateDate && b.CreateDate) {
+                return b.CreateDate.getTime() - a.CreateDate.getTime()
+            } else {
+                return 0
             }
-        } else if (
-            listAccessKeysForUser.AccessKeyMetadata[0].CreateDate &&
-            listAccessKeysForUser.AccessKeyMetadata[0].AccessKeyId &&
-            isKeyOutOfDate({
-                createDate: listAccessKeysForUser.AccessKeyMetadata[0].CreateDate,
-            })
-        ) {
-            try {
-                await sendMessage({
-                    accessKeyId: listAccessKeysForUser.AccessKeyMetadata[0].AccessKeyId,
-                    userName: userName || "unknown",
-                    region: event.Records[iterator].awsRegion,
-                    awsAccountId: context.invokedFunctionArn.split(":")[4],
-                })
-            } catch (error) {
-                console.log(error)
-                throw new Error("Couldn't send message to SQS")
+        })
+
+        for (const accessKey of sortedAccessKeys) {
+            if (accessKey.AccessKeyId && isKeyOutOfDate({ createDate: accessKey.CreateDate })) {
+                try {
+                    await sendMessage({
+                        accessKeyId: accessKey.AccessKeyId,
+                        userName: userName || "unknown",
+                        region: event.Records[iterator].awsRegion,
+                        awsAccountId: context.invokedFunctionArn.split(":")[4],
+                    })
+                    break
+                } catch (error) {
+                    console.log(error)
+                    throw new Error(`Couldn't send message to update access key for user: ${userName}`)
+                }
             }
         }
+
         iterator++
     }
     console.log("UserAccessLookUp finished")
