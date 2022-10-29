2d670f400a586f049f725cd3d8524ac2a0a7eba0
Nima Golzar Soufiani <nima.soufiani@rippleenergy.com>
2022-10-29 23:46:48 +0100

    rework user lookup to use sdk v3

--- src/functions/user-lookup/app.ts
+++ src/functions/user-lookup/app.ts
@@ -1,15 +1,16 @@
 import { ScheduledEvent } from "aws-lambda"
 
-import { IAM } from "@aws-sdk/client-iam"
-import { SQS } from "@aws-sdk/client-sqs"
+import { ListUsersCommand, IAMClient } from "@aws-sdk/client-iam"
+import { SendMessageCommand, SQSClient } from "@aws-sdk/client-sqs"
 
 export const lambdaHandler = async (event: ScheduledEvent) => {
-    const iam = new IAM({ region: event.region })
-    const sqs = new SQS({ region: event.region })
+    const iamClient = new IAMClient({ region: event.region })
+    const sqsClient = new SQSClient({ region: event.region })
     console.log("Starting user-lookup function")
 
     try {
-        const response = await iam.listUsers({})
+        const command = new ListUsersCommand({})
+        const response = await iamClient.send(command)
 
         if (!response.Users) {
             console.log("No users found")
@@ -21,8 +22,8 @@ export const lambdaHandler = async (event: ScheduledEvent) => {
                 MessageBody: JSON.stringify(user),
                 QueueUrl: `https://sqs.${event.region}.amazonaws.com/${event.account}/UserAccessKeyLookupQueue`,
             }
-
-            await sqs.sendMessage(message)
+            const sqsCommand = new SendMessageCommand(message)
+            await sqsClient.send(sqsCommand)
             console.log(`Sent message to queue for user ${user.UserName}`)
         }
     } catch (error) {
