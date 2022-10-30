ba7041def2ffc3cd0f67369e4f4f6e168b16cacf
Nima Golzar Soufiani <nima.soufiani@rippleenergy.com>
2022-10-30 15:23:45 +0000

    refactor send-email and store-secrets to use new sdk

--- src/functions/send-email/app.ts
+++ src/functions/send-email/app.ts
@@ -1,64 +1,61 @@
 import { SQSEvent } from "aws-lambda/trigger/sqs"
 
-import SES from "aws-sdk/clients/ses"
-import DynamoDB from "aws-sdk/clients/dynamodb"
-
-const ses = new SES()
-const dynamoDb = new DynamoDB.DocumentClient()
+import { SESClient, SendEmailCommand, SendEmailCommandInput } from "@aws-sdk/client-ses"
+import { DynamoDBClient, GetItemCommand, GetItemInput } from "@aws-sdk/client-dynamodb"
 
 export const lambdaHandler = async (event: SQSEvent) => {
-    return new Promise((resolve, reject) => {
-        event.Records.map(async (record) => {
-            const { UserName, Email } = JSON.parse(record.body)
-            if (!UserName || !Email) {
-                reject("No username or email found")
-            } else {
-                const params = {
-                    TableName: "User",
-                    Key: {
-                        name: UserName,
+    for (const record of event.Records) {
+        const sesClient = new SESClient({ region: record.awsRegion })
+        const dynamoDbClient = new DynamoDBClient({ region: record.awsRegion })
+
+        const { UserName }: { UserName: unknown } = JSON.parse(record.body)
+
+        if (typeof UserName !== "string") {
+            throw new Error("Invalid message")
+        }
+
+        const params: GetItemInput = {
+            TableName: "User",
+            Key: {
+                NAME: { N: UserName },
+            },
+        }
+
+        const getItemCommand = new GetItemCommand(params)
+
+        const response = await dynamoDbClient.send(getItemCommand)
+
+        if (!response?.Item?.Email.S) {
+            throw new Error("User email address not found in DynamoDB User table")
+        }
+
+        const emailParams: SendEmailCommandInput = {
+            Destination: {
+                ToAddresses: [response.Item.Email.S],
+            },
+            Message: {
+                Body: {
+                    Text: {
+                        Charset: "UTF-8",
+                        Data: `Your access key id is stored in Secrets Manager. Please use the following link to retrieve it: https://console.aws.amazon.com/secretsmanager/home?region=${record.awsRegion}#/secret?name=${UserName}-access-key`,
                     },
-                }
-                dynamoDb.get(params, (err, data) => {
-                    if (err) {
-                        reject(err)
-                    } else {
-                        const { Item } = data
-                        if (!Item) {
-                            reject("No item found")
-                        } else {
-                            const { accessKeyId, secretAccessKey } = Item
-                            const emailParams = {
-                                Destination: {
-                                    ToAddresses: [Email],
-                                },
-                                Message: {
-                                    Body: {
-                                        Text: {
-                                            Charset: "UTF-8",
-                                            Data: `Your access key id is ${accessKeyId} and your secret access key is ${secretAccessKey}`,
-                                        },
-                                    },
-                                    Subject: {
-                                        Charset: "UTF-8",
-                                        Data: "Your access key",
-                                    },
-                                },
-                                Source: "",
-                            }
-                            ses.sendEmail(emailParams, (err, data) => {
-                                if (err) {
-                                    console.log("Couldn't send email")
-                                    console.error(err)
-                                    reject(err)
-                                } else {
-                                    resolve(data)
-                                }
-                            })
-                        }
-                    }
-                })
-            }
-        })
-    })
+                },
+                Subject: {
+                    Charset: "UTF-8",
+                    Data: "Your access key",
+                },
+            },
+            Source: "",
+        }
+
+        const sendEmailCommand = new SendEmailCommand(emailParams)
+
+        try {
+            await sesClient.send(sendEmailCommand)
+        } catch (err) {
+            console.log("Couldn't send email")
+            console.error(err)
+            throw new Error("Couldn't send email")
+        }
+    }
 }
--- src/functions/store-secrets/app.ts
+++ src/functions/store-secrets/app.ts
@@ -1,11 +1,13 @@
 import { Context, SQSEvent } from "aws-lambda"
 
-import SNS from "aws-sdk/clients/sns"
-import SecretsManager from "aws-sdk/clients/secretsmanager"
-
-const sns = new SNS()
-
-const secretsManager = new SecretsManager()
+import { SNSClient, PublishCommand } from "@aws-sdk/client-sns"
+import {
+    SecretsManagerClient,
+    GetSecretValueCommand,
+    CreateSecretCommand,
+    PutResourcePolicyCommand,
+    UpdateSecretCommand,
+} from "@aws-sdk/client-secrets-manager"
 
 const sendSNSNotification = async ({
     region,
@@ -20,127 +22,96 @@ const sendSNSNotification = async ({
         Message: `User ${userName} has been updated`,
         TopicArn: `arn:aws:sns:${region}:${awsAccountId}:UpdatedAccessKeyAndStoredSecret`,
     }
+    const snsClient = new SNSClient({ region })
+    const snsCommand = new PublishCommand(snsParams)
 
-    sns.publish(snsParams, (err, data) => {
-        if (err) {
-            return Promise.reject(err)
-        } else {
-            console.log("sns notification sent")
-            return Promise.resolve(data)
-        }
-    })
+    try {
+        await snsClient.send(snsCommand)
+        console.log("sns notification sent")
+    } catch (err) {
+        console.log("Could not publish to SNS topic")
+        console.error(err)
+        throw new Error("Could not publish to SNS topic")
+    }
 }
 
 export const lambdaHandler = async (event: SQSEvent, context: Context) => {
     const awsAccountId = context.invokedFunctionArn.split(":")[4]
 
-    return new Promise((resolve, reject) => {
-        event.Records.map(async (record) => {
-            const parsedBody = JSON.parse(record.body)
-
-            const SecretsToStore = Object.entries(JSON.parse(record.body)).reduce(
-                (acc, [key, value]) => {
-                    if (key === "SecretId" || key == "Principle") {
-                        return {}
-                    }
-                    return { ...acc, [key]: value }
-                },
-                {}
-            )
-
-            secretsManager.getSecretValue(
-                {
-                    SecretId: parsedBody.SecretId,
-                },
-                (err, data) => {
-                    if (err) {
-                        secretsManager.createSecret(
-                            {
-                                Name: parsedBody.SecretId,
-                                SecretString: JSON.stringify({
-                                    ...SecretsToStore,
-                                }),
-                            },
-                            (err, data) => {
-                                if (err) {
-                                    console.error("Could not create secret")
-                                    console.error(err)
-                                    return reject(err)
-                                } else {
-                                    secretsManager.putResourcePolicy(
-                                        {
-                                            SecretId: parsedBody.SecretId,
-                                            ResourcePolicy: JSON.stringify({
-                                                Version: "2012-10-17",
-                                                Statement: [
-                                                    {
-                                                        Sid: "EnableIAMUserPermissions",
-                                                        Effect: "Allow",
-                                                        Principal: {
-                                                            AWS: `arn:aws:iam::${awsAccountId}:user/${parsedBody.UserName}`,
-                                                        },
-                                                        Action: [
-                                                            "secretsmanager:GetSecretValue",
-                                                            "secretsmanager:DescribeSecret",
-                                                            "secretsmanager:ListSecretVersionIds",
-                                                            "secretsmanager:GetResourcePolicy",
-                                                        ],
-                                                        Resource: data.ARN,
-                                                    },
-                                                    {
-                                                        Sid: "AllowListingOfSecrets",
-                                                        Effect: "Allow",
-                                                        Principal: {
-                                                            AWS: `arn:aws:iam::${awsAccountId}:user/${parsedBody.UserName}`,
-                                                        },
-                                                        Action: [
-                                                            "secretsmanager:GetRandomPassword",
-                                                            "secretsmanager:ListSecrets",
-                                                        ],
-                                                        Resource: "*",
-                                                    },
-                                                ],
-                                            }),
-                                        },
-                                        (err, data) => {
-                                            if (err) {
-                                                console.error("Error putting resource policy")
-                                                return reject(err)
-                                            }
-                                            sendSNSNotification({
-                                                awsAccountId,
-                                                region: record.awsRegion,
-                                                userName: parsedBody.UserName,
-                                            })
-                                        }
-                                    )
-                                }
-                            }
-                        )
-                    } else {
-                        secretsManager.updateSecret(
-                            {
-                                SecretId: parsedBody.SecretId,
-                                SecretString: JSON.stringify({
-                                    ...SecretsToStore,
-                                }),
-                            },
-                            (err, data) => {
-                                if (err) {
-                                    console.error("Error updating secret")
-                                    console.error(err)
-                                    return reject(err)
-                                }
-                                sendSNSNotification({
-                                    awsAccountId,
-                                    region: record.awsRegion,
-                                    userName: parsedBody.UserName,
-                                })
-                            }
-                        )
-                    }
-                }
-            )
+    for (const record of event.Records) {
+        const secretsManagerClient = new SecretsManagerClient({ region: record.awsRegion })
+        const parsedBody = JSON.parse(record.body)
+        const SecretsToStore = Object.entries(JSON.parse(record.body)).reduce((acc, [key, value]) => {
+            if (key === "SecretId" || key == "Principle") {
+                return {}
+            }
+            return { ...acc, [key]: value }
+        }, {})
+        const command = new GetSecretValueCommand({
+            SecretId: parsedBody.SecretId,
         })
-    })
+
+        try {
+            await secretsManagerClient.send(command)
+            const updateSecretCommand = new UpdateSecretCommand({
+                SecretId: parsedBody.SecretId,
+                SecretString: JSON.stringify({
+                    ...SecretsToStore,
+                }),
+            })
+            await secretsManagerClient.send(updateSecretCommand)
+            sendSNSNotification({
+                region: record.awsRegion,
+                awsAccountId,
+                userName: parsedBody.UserName,
+            })
+        } catch (error) {
+            const createSecretCommand = new CreateSecretCommand({
+                Name: parsedBody.SecretId,
+                SecretString: JSON.stringify({
+                    ...SecretsToStore,
+                }),
+            })
+
+            const secretsManagerResponse = await secretsManagerClient.send(createSecretCommand)
+
+            const putResourcePolicyCommand = new PutResourcePolicyCommand({
+                SecretId: parsedBody.SecretId,
+                ResourcePolicy: JSON.stringify({
+                    Version: "2012-10-17",
+                    Statement: [
+                        {
+                            Sid: "EnableIAMUserPermissions",
+                            Effect: "Allow",
+                            Principal: {
+                                AWS: `arn:aws:iam::${awsAccountId}:user/${parsedBody.UserName}`,
+                            },
+                            Action: [
+                                "secretsmanager:GetSecretValue",
+                                "secretsmanager:DescribeSecret",
+                                "secretsmanager:ListSecretVersionIds",
+                                "secretsmanager:GetResourcePolicy",
+                            ],
+                            Resource: secretsManagerResponse.ARN,
+                        },
+                        {
+                            Sid: "AllowListingOfSecrets",
+                            Effect: "Allow",
+                            Principal: {
+                                AWS: `arn:aws:iam::${awsAccountId}:user/${parsedBody.UserName}`,
+                            },
+                            Action: ["secretsmanager:GetRandomPassword", "secretsmanager:ListSecrets"],
+                            Resource: "*",
+                        },
+                    ],
+                }),
+            })
+            await secretsManagerClient.send(putResourcePolicyCommand)
+            sendSNSNotification({
+                region: record.awsRegion,
+                awsAccountId,
+                userName: parsedBody.UserName,
+            })
+        }
+    }
 }
