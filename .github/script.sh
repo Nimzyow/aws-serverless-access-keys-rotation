2edca1757ea270714da4705ce424116d6c79ae50
Nima Golzar Soufiani <nima.soufiani@rippleenergy.com>
2022-10-30 20:16:06 +0000

    this script has been tested so surely it has to work now

--- .github/script.sh
+++ .github/script.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-for d in ./src/functions/ ; do (cd "$d" && yarn install && echo "insalled packages for $d"); done
-ls
-echo "end?"
+for d in ./src/functions/* ; do (cd "$d" && yarn install && echo "insalled packages for $d"); done
+echo "All function packages have been intsalled"
