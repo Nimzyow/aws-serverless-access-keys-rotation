955b5d8f6b51d5c00ae00a2732f14296a0c7f3ae
Nima Golzar Soufiani <nima.soufiani@rippleenergy.com>
2022-10-30 20:09:01 +0000

    learning some things about scripting?

--- .github/script.sh
+++ .github/script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-for d in ./src/functions/ ; do (cd "$d" && yarn install); done
+for d in ./src/functions/ ; do (cd "$d" && yarn install && echo "insalled packages for $d"); done
 ls
 echo "end?"
