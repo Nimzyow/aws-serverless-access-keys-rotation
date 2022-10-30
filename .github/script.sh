1b05950e44595948441294f158a4c1f6b3c31629
Nima Golzar Soufiani <nima.soufiani@rippleenergy.com>
2022-10-30 20:38:28 +0000

    correct typpo

--- .github/script.sh
+++ .github/script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-for d in ./src/functions/* ; do (cd "$d" && yarn install && echo "insalled packages for $d"); done
-echo "All function packages have been intsalled"
+for directory in ./src/functions/* ; do (cd "$directory" && yarn install && echo "insalled packages for $directory"); done
+echo "All function packages have been installed"
